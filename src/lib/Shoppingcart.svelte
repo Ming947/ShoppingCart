023f44fa7d1b74bcc1c0da723bc7beff894cb033
mliu947 <mingmingliu3517@gmail.com>
2024-09-20 17:31:16 +1200

    wow

---
+++ src/lib/Shoppingcart.svelte
@@ -0,0 +1,14 @@
+<script>
+    import ProductItem from "./components/ProductItem.svelte";
+
+    let productToAdd = null;
+
+    function addItem(e){
+        productToAdd = e.detail;
+        console.log(productToAdd);
+    }
+
+</script>
+
+<ProductItem on:itemClicked={addItem}/>
+<p>Let's buy: {productToAdd}</p>
